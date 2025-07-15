2fa26f06b2810b10f87ce269c1f48c625f094196
artazeon <artazeon@gmail.com>
2025-07-15 18:45:07 +0300

    Skeleton Pizza

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import Header from './components/Header/Header'
 import Categories from './components/Categories/Categories'
 import Sort from './components/Sort/Sort'
 import PizzaBlock from './components/PizzaBlock/PizzaBlock'
+import Skeleton from './components/PizzaBlock/Skeleton'
 
 function App() {
   const [items, setItems] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
     fetch('https://7a864f3f9ff03705.mokky.dev/items')
       .then((res) => res.json())
-      .then((arr) => setItems(arr))
+      .then((arr) => {
+        setItems(arr)
+        setIsLoading(false)
+      })
   }, [])
 
   return (
@@ -26,9 +31,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj, index) => (
-              <PizzaBlock {...obj} key={index} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj, index) => <PizzaBlock {...obj} key={index} />)}
           </div>
         </div>
       </div>
